373d7c80c3c214c3cd6d7763c2f4615b674b0c1a
Kyle Wilson <kyle@ninecrows.com>
2025-03-21 07:39:42 -0400

    Updates to command line tool version

--- SampleEchoApp/MainWindow.xaml.cs
+++ SampleEchoApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Net.Cache;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,5 +22,34 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        Loaded += GetAddresses;
+       
+        TextIpAddress.Text = "127.0.0.1";
+    }
+
+    private async void GetAddresses(object e, RoutedEventArgs a)
+    {
+        HttpClient client = new HttpClient();
+        var response = await client.GetAsync("http://checkip.dyndns.org");
+
+        //response.EnsureSuccessStatusCode()
+        // .WriteRequestToConsole();
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+
+        var pattern = new Regex(@"(\d+\.\d+\.\d+\.\d+)");
+        var matches = pattern.Matches(jsonResponse);
+
+        TextMessages.Text += $"Received \"{jsonResponse}\"";
+
+        if (matches.Count > 0)
+        {
+            TextIpAddress.Text = matches[matches.Count - 1].Value;
+        }
+        else
+        {
+            TextIpAddress.Text = "Unknown";
+        }
     }
 }
